a27c4c3e444600e1e86987575d7bf99fc12e218f
Italo Abreu <italodw@gmail.com>
2020-05-18 18:26:48 -0300

    medicos registratd na administração

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from core.models import Especialidade
+from core.models import Medico
 
 class EspecialidadeAdmin(admin.ModelAdmin):
     list_display = ['nome']
 
+
+class MedicoAdmin(admin.ModelAdmin):
+    list_display = ['crm', 'nome', 'email', 'especialidade']
+
 admin.site.register(Especialidade, EspecialidadeAdmin)
+admin.site.register(Medico, MedicoAdmin)
