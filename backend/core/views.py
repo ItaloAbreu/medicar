c543647a06f57ef77f3441a0987d44d25a0b70b7
Italo Abreu <italodw@gmail.com>
2020-05-18 18:15:18 -0300

    filtro melhorado

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
 from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import FilterSet
+from django_filters.filters import Filter
+from django_filters.fields import Lookup
 from core.models import Especialidade
 from core.models import Medico
 from core.serializers import EspecialidadeSerializer
@@ -14,9 +17,17 @@ class EspecialidadeViewSet(viewsets.ModelViewSet):
     search_fields = ('nome',)
 
 
+class MedicoFilterSet(FilterSet):
+
+    class Meta:
+        model = Medico
+        fields = ['especialidade']
+
+
 class MedicoViewSet(viewsets.ModelViewSet):
     queryset = Medico.objects.all()
     serializer_class = MedicoSerializer
     filter_backends = (filters.SearchFilter, DjangoFilterBackend,)
+    filter_class = MedicoFilterSet
     filterset_fields = ['especialidade']
     search_fields = ('nome',)
