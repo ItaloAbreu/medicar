a6ff7f7541964b7615a8b4b68c3c80b10d59e940
Italo Abreu <italodw@gmail.com>
2020-05-18 22:16:51 -0300

    Autenticação

--- backend/medicar/settings.py
+++ backend/medicar/settings.py
@@ -144,7 +144,11 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
 }
 
 # ie if Heroku server
